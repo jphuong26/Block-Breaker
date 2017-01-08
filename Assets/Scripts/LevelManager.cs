711aa84cbc4a8470f477e466be5ea3a568b6fcf1
Julie Phuong <jphuong26@gmail.com>
2017-01-08 14:03:44 +1030

    added basic flow of buttons

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void GoToLevel (string level) {
+		Application.LoadLevel(level);
+	}
+
+	public void ExitLevel () {
+		Application.Quit();
+	}
+}
