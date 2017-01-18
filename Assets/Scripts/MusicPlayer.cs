7b7411ae9a675ac5b68556911790480b6ace4f2d
Julie Phuong <jphuong26@gmail.com>
2017-01-18 22:01:54 +1030

    Removing Assembly-CSharp.csproj

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,17 +5,15 @@ public class MusicPlayer : MonoBehaviour {
 
 	static MusicPlayer instance = null;
 
-	// Use this for initialization
-	void Start () {
+	void Awake () {
 		if (instance != null) {
 			Destroy (gameObject);
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
-
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
